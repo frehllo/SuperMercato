8d31224bb03f6fc324a8c3a6f45ff0e02d46a3d6
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-16 12:15:22 +0200

    Add project files.

---
+++ SuperMercato/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SuperMercato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            double prezzofinale = 0;
+            double sconto;
+            double tot = 0;
+            Console.Write("Quanto ha speso? ");
+            tot = int.Parse(Console.ReadLine());
+            if(tot > 100)
+            {
+                sconto = (tot / 100) * 20;
+                prezzofinale = tot - sconto;
+                Console.WriteLine($"Hai speso più di 100 euro, applicando uno sconto paghi {prezzofinale} euro");
+                Console.ReadLine();
+            }else
+            {
+                Console.WriteLine("Niente sconto, arrivederci e grazie :)");
+                Console.ReadLine();
+            }
+        }
+    }
+}
